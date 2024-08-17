0b257e3d3a0d1c89d62aec93e644a991037418b8
Efe Dönmez <efdo@halia.com.tr>
2024-08-17 16:02:53 +0300

    all

---
+++ pdks-halia.Core/Contracts/Services/ISampleDataService.cs
@@ -0,0 +1,13 @@
+﻿using pdks_halia.Core.Models;
+
+namespace pdks_halia.Core.Contracts.Services;
+
+// Remove this class once your pages/features are using your data.
+public interface ISampleDataService
+{
+    Task<IEnumerable<SampleOrder>> GetContentGridDataAsync();
+
+    Task<IEnumerable<SampleOrder>> GetGridDataAsync();
+
+    Task<IEnumerable<SampleOrder>> GetListDetailsDataAsync();
+}
